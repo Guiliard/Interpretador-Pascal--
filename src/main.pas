64f386394803af464c651ed6bf9a2720d370bb61
Guiliard <camposrafa806@gmail.com>
2025-04-19 16:37:54 -0300

    refactor main

--- src/main.pas
+++ src/main.pas
@@ -1,13 +1,29 @@
 program main;
 
 uses
-  reader;
+  reader,      
+  analyzer_final,    
+  lexeme_final,    
+  type_token,  
+  utils;       
 
 var
   fileContent: string;
+  lexemes: array of lexeme;
+  i: Integer;
 
 begin
   fileContent := ReadFileToString;
   Writeln('Conteúdo do arquivo:');
   Writeln(fileContent);
+  
+  Writeln('Analisando o conteúdo do arquivo...');
+  lexemes := analyzer(fileContent);
+  Writeln('Análise concluída.');
+
+  Writeln('--- Lista de lexemas ---');
+  for i := 0 to High(lexemes) do
+  begin
+    Writeln('Texto: ', lexemes[i].lex_text, ' | Token: ', getTokenName(lexemes[i].token_real), ' | Linha: ', lexemes[i].line, ' | Coluna: ', lexemes[i].column);
+  end;
 end.
