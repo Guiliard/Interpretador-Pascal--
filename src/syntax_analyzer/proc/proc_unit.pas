d0f6627ba3275f8e0a61fcb32c067a90fc863d13
Guiliard <camposrafa806@gmail.com>
2025-05-07 16:22:18 -0300

    add more procs

--- src/syntax_analyzer/proc/proc_unit.pas
+++ src/syntax_analyzer/proc/proc_unit.pas
@@ -23,4 +23,52 @@ begin
     eatToken(lexemes, i, type_token_unit._DOT_);
 end;
 
-end.
+procedure procDeclarations(lexemes: lexeme_array; var i: integer);
+begin
+    eatToken(lexemes, i, type_token_unit._VAR_);
+    procDeclaration(lexemes, i);
+    procRestDeclaration(lexemes, i);
+end;
+
+procedure procDeclaration(lexemes: lexeme_array; var i: integer);
+begin
+    procListIdent(lexemes, i);
+    eatToken(lexemes, i, type_token_unit._COLON_); 
+    procType(lexemes, i);
+    eatToken(lexemes, i, type_token_unit._SEMICOLON_);
+end;
+
+procedure procListIdent(lexemes: lexeme_array; var i: integer);
+begin
+    eatToken(lexemes, i, type_token_unit._VARIABLE_);
+    procRestListIdent(lexemes, i);
+end;
+
+procedure procRestListIdent(lexemes: lexeme_array; var i: integer);
+begin
+    if lexemes[i].token_real = type_token_unit._COMMA_ then
+    begin
+        eatToken(lexemes, i, type_token_unit._COMMA_);    
+        eatToken(lexemes, i, type_token_unit._VARIABLE_); 
+        procRestListIdent(lexemes, i);                    
+    end;
+end;
+
+procedure procRestDeclaration(lexemes: lexeme_array; var i: integer);
+begin
+    if lexemes[i].token_real = type_token_unit._VARIABLE_ then
+    begin
+        procDeclaration(lexemes, i);   
+        procRestDeclaration(lexemes, i);
+    end;
+end;
+
+procedure procType(lexemes: lexeme_array; var i: integer);
+const 
+    typeSet: set of type_token_unit = [type_token_unit._INTEGER_, type_token_unit._REAL_, type_token_unit._STRING_];
+begin
+    if lexemes[i].token_real in typeSet then
+    begin
+        eatToken(lexemes, i, lexemes[i].token_real);
+    end;
+end;
