1aa60c1a99617dcd61c30c057c9f07206309bcab
Guiliard <camposrafa806@gmail.com>
2025-05-07 14:59:25 -0300

    new main

--- src/main.pas
+++ src/main.pas
@@ -1,26 +1,19 @@
 program main;
 
 uses
-  reader_unit,      
-  lexical_analyzer_unit,    
-  lexeme_unit,    
-  lexical_utils_unit;       
+  reader_unit, 
+  lexeme_unit,     
+  lexical_analyzer_unit,
+  syntax_analyzer_unit;   
 
 var
   fileContent: AnsiString;
-  lexemes: array of lexeme;
-  i: Integer;
+  lexemes: lexeme_array;
 
 begin
   fileContent := ReadFileToString;
-  Writeln('--- Conteúdo do arquivo ---');
-  Writeln(fileContent);
   
   lexemes := lexicalAnalyzer(fileContent);
 
-  Writeln('--- Lista de lexemas ---');
-  for i := 0 to High(lexemes) do
-  begin
-    Writeln('Texto: ', lexemes[i].lex_text, ' | Token: ', getTokenName(lexemes[i].token_real), ' | Linha: ', lexemes[i].line, ' | Coluna: ', lexemes[i].column);
-  end;
+  syntaxAnalyzer(lexemes);
 end.
