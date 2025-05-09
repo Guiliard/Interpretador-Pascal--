7c727a3e62550e0dcd007af52d64a655ab3ee109
vramoscabral <vramos@VitaoPC22>
2025-05-09 16:03:59 -0300

    stmt added

--- src/syntax_analyzer/proc/proc_unit.pas
+++ src/syntax_analyzer/proc/proc_unit.pas
@@ -72,3 +72,60 @@ begin
         eatToken(lexemes, i, lexemes[i].token_real);
     end;
 end;
+
+procedure procStmtList(lexemes: lexeme_array; var i: integer);
+const
+    stmtSet: set of type_token_unit = [
+    type_token_unit._FOR_, type_token_unit._READ_, type_token_unit._WRITE_,
+    type_token_unit._READLN_, type_token_unit._WRITELN_, type_token_unit._WHILE_,
+    type_token_unit._VARIABLE_, type_token_unit._IF_, type_token_unit._BEGIN_,
+    type_token_unit._BREAK_, type_token_unit._CONTINUE_, type_token_unit._SEMICOLON_];
+begin
+    if lexemes[i].token_real in stmtSet then
+    begin
+        procStmt(lexemes, i);   
+        procStmtList(lexemes, i);
+    end;
+end;
+
+procedure procStmt(lexemes: lexeme_array; var i: integer);
+begin
+    if lexemes[i].token_real = type_token_unit._FOR_ then
+    begin
+        procForStmt(lexemes, i);
+    end
+    else if lexemes[i].token_real in [type_token_unit._READ_, type_token_unit._WRITE_, type_token_unit._READLN_, type_token_unit._WRITELN_] then
+    begin
+        procIoStmt(lexemes, i);
+    end
+    else if lexemes[i].token_real = type_token_unit._WHILE_ then
+    begin
+        procWhileStmt(lexemes, i);
+    end
+    else if lexemes[i].token_real = type_token_unit._VARIABLE_ then
+    begin
+        procAtrib(lexemes, i);
+    end
+    else if lexemes[i].token_real = type_token_unit._IF_ then
+    begin
+        procIfStmt(lexemes, i);
+    end
+    else if lexemes[i].token_real = type_token_unit._BEGIN_ then
+    begin
+        procBlock(lexemes, i);
+    end
+    else if lexemes[i].token_real = type_token_unit._BREAK_ then
+    begin
+        eatToken(lexemes, i, type_token_unit._BREAK_);
+        eatToken(lexemes, i, type_token_unit._SEMICOLON_);
+    end
+    else if lexemes[i].token_real = type_token_unit._CONTINUE_ then
+    begin
+        eatToken(lexemes, i, type_token_unit._CONTINUE_);
+        eatToken(lexemes, i, type_token_unit._SEMICOLON_);
+    end
+    else if lexemes[i].token_real = type_token_unit._SEMICOLON_ then
+    begin
+        eatToken(lexemes, i, type_token_unit._SEMICOLON_);
+    end
+end;
