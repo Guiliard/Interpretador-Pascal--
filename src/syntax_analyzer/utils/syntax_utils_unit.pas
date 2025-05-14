e23f989927eb7f4b399e070e5ad2b703555c735a
Guiliard <camposrafa806@gmail.com>
2025-05-14 15:31:45 -0300

    functions

--- src/syntax_analyzer/utils/syntax_utils_unit.pas
+++ src/syntax_analyzer/utils/syntax_utils_unit.pas
@@ -6,6 +6,8 @@ uses
     type_token_unit,
     lexeme_unit;
 
+procedure advanceToken(lexemes: lexeme_array; var i: integer);
+procedure showError(currentLexeme: lexeme);
 procedure eatToken(lexemes: lexeme_array; var i: integer; token: typeToken);
 
 implementation
@@ -30,21 +32,7 @@ end;
 
 procedure showError(currentLexeme: lexeme);
 begin 
-    if (currentLexeme.token_real = type_token_unit._INVALID_TOKEN_) then
-    begin
-        writeln('Error: Invalid token at line ', currentLexeme.line, ', column ', currentLexeme.column, ', Token: ', currentLexeme.lex_text, '.', #10);
-    end
-
-    else
-    if (currentLexeme.token_real = type_token_unit._END_OF_FILE_) then
-    begin
-        writeln('Error: Unexpected end of file.', #10);
-    end
-    
-    else
-    begin
-        writeln('Error: Unexpected token at line ', currentLexeme.line, ', column ', currentLexeme.column, ', Token: ', currentLexeme.lex_text, '.', #10);
-    end;
+    writeln(#10, 'Syntax Error: Unexpected token at line ', currentLexeme.line, ', column ', currentLexeme.column, '. The token: "', currentLexeme.lex_text, '" is not appropriate.', #10);
 end;
 
 end.
