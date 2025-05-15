59bd92599f0e138c649a23aa77ca23ca30de632f
Guiliard <camposrafa806@gmail.com>
2025-05-15 14:14:45 -0300

    finished the second part

--- src/syntax_analyzer/utils/syntax_utils_unit.pas
+++ src/syntax_analyzer/utils/syntax_utils_unit.pas
@@ -6,28 +6,31 @@ uses
     type_token_unit,
     lexeme_unit;
 
-procedure advanceToken(lexemes: lexeme_array; var i: integer);
+procedure advanceToken(var i: integer);
 procedure showErrorSyntax(currentLexeme: lexeme; token: typeToken);
 procedure eatToken(lexemes: lexeme_array; var i: integer; token: typeToken);
 
 implementation
 
-procedure advanceToken(lexemes: lexeme_array; var i: integer);
+procedure advanceToken(var i: integer);
 begin 
-    if (i <= High(lexemes)) then
-    begin
-        Inc(i);
-    end;
+    Inc(i);
 end;
 
 procedure eatToken(lexemes: lexeme_array; var i: integer; token: typeToken);
 begin 
+    if (i = High(lexemes)) then
+    begin
+        writeln(#10, 'Syntax Error: Unexpected end of file at line ', lexemes[i-1].line, '.', #10);
+        Halt(1);
+    end 
+    else 
     if (lexemes[i].token_real <> token) then
     begin 
         showErrorSyntax(lexemes[i], token);
         Halt(1);
     end;
-    advanceToken(lexemes, i)
+    advanceToken(i)
 end;
 
 procedure showErrorSyntax(currentLexeme: lexeme; token: typeToken);
