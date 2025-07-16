48aded1ec5c58bea373b092c48bbd869f63dbb6a
Guiliard <camposrafa806@gmail.com>
2025-07-15 22:31:21 -0300

    main

--- src/main.pas
+++ src/main.pas
@@ -1,36 +1,36 @@
 program main;
 
 uses
-  reader_unit, 
+	reader_unit, 
 	lexeme_unit,     
 	lexical_analyzer_unit,
 	syntax_analyzer_unit, 
 	intermediate_code_unit,
-	intermediate_utils_unit;
-  itm_analyzer,
-  itm_runner,
-  itm_reader;
+	intermediate_utils_unit,
+	itm_analyzer,
+	itm_runner,
+	itm_reader;
 
 var
-  fileContent: AnsiString;
-  lexemes: lexeme_array;
-  arrayCode: intermediate_code_array;
-  itmContent: AnsiString;
-  itms: ItmArray;
+	fileContent: AnsiString;
+	lexemes: lexeme_array;
+	arrayCode: intermediate_code_array;
+	itmContent: AnsiString;
+	itms: ItmArray;
 
 begin
 	fileContent := ReadFileToString;
 
 	lexemes := lexicalAnalyzer(fileContent);
 
-  arrayCode := syntaxAnalyzer(lexemes);
+  	arrayCode := syntaxAnalyzer(lexemes);
 
 	printArrayCode(arrayCode);
 
 	writeln('Syntax analysis completed successfully. No errors found.');
 
-  itmContent := readItmFile;
-  itms := analyzeItm(itmContent);
+	itmContent := readItmFile;
+	itms := analyzeItm(itmContent);
 
-  RunInstructions(itms);
+  	RunInstructions(itms);
 end.
